c96dfe04dfbaebfe0b0b3256dde4a7080a410a63
loriahpope <llp260@nyu.edu>
2014-08-13 10:39:43 -0400

    added some CSS changes

--- app/controllers/record_expenses_controller.rb
+++ app/controllers/record_expenses_controller.rb
@@ -61,6 +61,11 @@ class RecordExpensesController < ApplicationController
     end
   end
 
+
+  def say_hello(name)
+    "Hello, #{name}."
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_record_expense
---
+++ config/initializers/formats.rb
@@ -0,0 +1,2 @@
+Date::DATE_FORMATS[:default]="%m/%d/%y"
+Time::DATE_FORMATS[:default]="%Y/%m/%d %H:%M"
